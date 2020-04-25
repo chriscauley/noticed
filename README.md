3fe2055bb3659ed34507ca8da4a59fb9b8d69fb8
chriscauley <chris@lablackey.com>
2020-04-25 11:49:25 +0000

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noticed
+
+An app for sharing business hours affected by the 2020 quarantine.
---
+++ server/settings/10-apps.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'server',
+    'unrest.nopass',
+    'unrest.user',
+]
+
+AUTH_USER_MODEL = 'user.User'

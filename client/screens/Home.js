4f688982d7b5eed125cc15471403178b3203a279
chriscauley <chris@lablackey.com>
2020-04-25 23:55:22 +0000

    location hook (testing on home screen)

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,1 +1,27 @@
-export default () => 'HOME!!'
+import React from 'react'
+import { withLocation } from '../components/Location'
+
+export default withLocation((props) => {
+  const { location, saveLocation } = props
+
+  if (!location) {
+    const success = (position) => {
+      const { latitude, longitude } = position.coords
+
+      saveLocation({ latitude, longitude })
+    }
+    const error = (e) => console.error(e)
+    navigator.geolocation.getCurrentPosition(success, error)
+    return <div>We need to know your location!</div>
+  }
+  const { latitude, longitude } = location
+
+  const href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`
+  const text = `Latitude: ${latitude} °, Longitude: ${longitude} °`
+  return (
+    <div>
+      <a href={href}>{text}</a>
+      <button onClick={() => saveLocation(null)}>clear location</button>
+    </div>
+  )
+})

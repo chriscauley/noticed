0514c5dfd64ab83f8bce1203bdfe14a559c34690
chriscauley <chris@lablackey.com>
2020-04-25 14:20:24 +0000

    adding postgis database

--- server/settings/00-base.py
+++ server/settings/00-base.py
@@ -45,10 +45,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'noticed',
+    },
 }
 
 
--- server/settings/10-apps.py
+++ server/settings/10-apps.py
@@ -6,6 +6,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.gis',
     'server',
     'unrest.nopass',
     'unrest.user',

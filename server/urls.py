b59c9e0cae12754a7e11ec4ef8db271ec7ae2e14
chriscauley <chris@lablackey.com>
2020-04-26 18:55:24 +0000

    serve media files when in development

--- server/urls.py
+++ server/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, re_path, include
@@ -14,3 +15,12 @@ urlpatterns = [
     re_path('^$', unrest_views.index),
     re_path('^api/auth/', include('unrest.user.urls')),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True
+        }),
+    ]

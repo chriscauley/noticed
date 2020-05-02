5df43169ffb9a34b66e81b2970ec76c144b688ab
chriscauley <chris@lablackey.com>
2020-05-02 19:17:38 +0000

    moving all bin scripts to ur repo

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
 
 def main():
@@ -11,6 +12,10 @@ def main():
         raise ImportError("Couldn't import Django. Are you sure it's installed and "
                           "available on your PYTHONPATH environment variable? Did you "
                           "forget to activate a virtual environment?") from exc
+
+    runserver.default_port = "8396"
+    runserver.default_addr = "0.0.0.0"
+
     execute_from_command_line(sys.argv)
 
 

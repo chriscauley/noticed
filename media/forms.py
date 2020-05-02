6b69e98719bfaf5714b6ecdbee9bdef97a6fb08c
chriscauley <chris@lablackey.com>
2020-05-02 14:06:03 +0000

    location_id should be treated as a raw_id to avoid returning thousands of choices

--- media/forms.py
+++ media/forms.py
@@ -4,10 +4,12 @@ from media.models import Photo
 
 
 class PhotoForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['location'].required = False
-
+    location_id = forms.IntegerField(required=False)
+    def clean_location_id(self):
+        location_id = self.cleaned_data.get('location_id')
+        if location_id and not Location.objects.filter(id=location_id):
+            raise forms.ValidationError(f"Location with id #{location_id} does not exist")
+        return location_id
     def save(self, commit=True):
         photo = super().save(commit=False)
         self.instance.user = self.request.user
@@ -16,4 +18,4 @@ class PhotoForm(forms.ModelForm):
 
     class Meta:
         model = Photo
-        fields = ('src', 'location')
+        fields = ('src', 'location_id')
